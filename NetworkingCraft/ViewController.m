e742e17c603ccb3f21cea7f817d8eaec3d6f8e79
YouXianMing <705786299@qq.com>
2015-06-30 14:31:05 +0800

    1.8
    完善断点下载的例子

--- NetworkingCraft/ViewController.m
+++ NetworkingCraft/ViewController.m
@@ -13,6 +13,7 @@
 #import "WeatherNetwork.h"
 #import "DownloadTask.h"
 #import "AFDownloadRequestOperation.h"
+#import "GCD.h"
 
 #define  UPLOAD_PIC_STR  @"UPLOAD_PIC_STR"
 #define  WEATHER_DATA    @"WEATHER_DATA"
@@ -33,7 +34,7 @@
     [super viewDidLoad];
     
     // 网络请求
-    [self normalRequest];
+//    [self normalRequest];
     
     // 下载任务
 //    [self downloadTaskRequest];
@@ -132,16 +133,47 @@
 - (void)resumeDownload {
     
     NSLog(@"%@", [NSHomeDirectory() stringByAppendingPathComponent:nil]);
-    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://41.duote.com.cn/2345explorer.exe"]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://29.duote.org/antiarp.zip"]];
     AFDownloadRequestOperation *operation = [[AFDownloadRequestOperation alloc] initWithRequest:request
-                                                                                 fileIdentifier:@"2345explorer.exe"
-                                                                                     targetPath:[NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Caches"]
+                                                                                 fileIdentifier:@"network.zip"
+                                                                                     targetPath:[NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Caches/network.zip"]
                                                                                    shouldResume:YES];
+    operation.shouldOverwrite = YES;
+
+    
+    // 开始下载
     [operation start];
     
+    // 4s后暂停
+    [GCDQueue executeInMainQueue:^{
+        NSLog(@"暂停");
+        [operation pause];
+    } afterDelaySecs:4.f];
+    
+    // 7s后继续恢复
+    [GCDQueue executeInMainQueue:^{
+        NSLog(@"开始");
+        [operation resume];
+    } afterDelaySecs:7.f];
+
+    // 查看下载进度
     [operation setProgressiveDownloadProgressBlock:^(AFDownloadRequestOperation *operation, NSInteger bytesRead, long long totalBytesRead, long long totalBytesExpected, long long totalBytesReadForFile, long long totalBytesExpectedToReadForFile) {
-        NSLog(@"%f", (float)totalBytesRead / (float)totalBytesExpected);
+        
+        CGFloat percent = (float)totalBytesReadForFile / (float)totalBytesExpectedToReadForFile;
+        NSLog(@"%f %ld  %lld  %lld  %lld", percent, (long)bytesRead, totalBytesRead, totalBytesReadForFile, totalBytesExpectedToReadForFile);
     }];
+    
+    // 结束block
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        NSLog(@"下载成功 %@", responseObject);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        NSLog(@"下载失败 %@", error);
+        
+    }];
+    
 }
 
 #pragma mark - 加载图片
